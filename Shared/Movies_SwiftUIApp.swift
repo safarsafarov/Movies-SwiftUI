b2851bfeacb196f0b95719d070b96fef02ba60da
Safar Safarov <Safarov.safar@hotmail.com>
2022-08-22 23:47:53 +0500

    ✨

---
+++ Shared/Movies_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Movies_SwiftUIApp.swift
+//  Shared
+//
+//  Created by Safar Safarov on 22/08/22.
+//
+
+import SwiftUI
+
+@main
+struct Movies_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
